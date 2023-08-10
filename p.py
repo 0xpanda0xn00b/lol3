1ebd4fa6a536d29281f36c0e48c93e4caf6aa933
root <0xn00bpwn@gmail.com>
2023-08-09 23:40:41 -0400

    lol first

---
+++ p.py
@@ -0,0 +1,8 @@
+
+from pyvirtualdisplay import Display
+import os
+
+display = Display(visible=0, size=(1024,1024)).start()
+
+
+os.system("/root/EXTRAT/firefox-97.0.1/firefox/firefox https://www.tiktok.com/@s_6o6/live")
